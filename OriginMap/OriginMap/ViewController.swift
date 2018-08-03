61c342592475dc1c335cd98c27089c36e082a266
ICloud <zhengyun@yeah.net>
2018-08-03 18:01:05 +0800

    ====

--- OriginMap/OriginMap/ViewController.swift
+++ OriginMap/OriginMap/ViewController.swift
@@ -12,8 +12,11 @@ import CoreLocation
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    
+    var objectAnnotation: MKPointAnnotation?
     let locationManager:CLLocationManager = CLLocationManager()
+    var isLight: Bool = false
+    let la = 31.846409999999999
+    let lo = 117.198775
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,32 +24,33 @@ class ViewController: UIViewController {
         mapView.mapType = MKMapType.standard
         
         //创建一个MKCoordinateSpan对象，设置地图的范围（越小越精确）
-        var latDelta = 0.05
-        var longDelta = 0.05
-        var currentLocationSpan: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let latDelta = 0.05
+        let longDelta = 0.05
+        let currentLocationSpan: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
         
         //定义地图区域和中心坐标（
         //使用当前位置
         //var center:CLLocation = locationManager.location.coordinate
         //使用自定义位置
-        var center: CLLocation = CLLocation(latitude: 32.029171, longitude: 118.788231)
+        let center: CLLocation = CLLocation(latitude: la, longitude: lo)
         
-        var currentRegion: MKCoordinateRegion = MKCoordinateRegion(center: center.coordinate,
+        let currentRegion: MKCoordinateRegion = MKCoordinateRegion(center: center.coordinate,
                                                                   span: currentLocationSpan)
         
         //设置显示区域
         mapView.setRegion(currentRegion, animated: true)
         
         //创建一个大头针对象
-        var objectAnnotation = MKPointAnnotation()
+        objectAnnotation = MKPointAnnotation()
         //设置大头针的显示位置
-        objectAnnotation.coordinate = CLLocation(latitude: 32.029171, longitude: 118.788231).coordinate
+        objectAnnotation?.coordinate = CLLocation(latitude: la, longitude: lo).coordinate
         //设置点击大头针之后显示的标题
-        objectAnnotation.title = "南京夫子庙"
-        //设置点击大头针之后显示的描述
-        objectAnnotation.subtitle = "南京市秦淮区秦淮河北岸中华路"
+//        objectAnnotation.title = "南京夫子庙"
+//        //设置点击大头针之后显示的描述
+//        objectAnnotation.subtitle = "南京市秦淮区秦淮河北岸中华路"
         //添加大头针
-        mapView.addAnnotation(objectAnnotation)
+        objectAnnotation?.title = ""
+        mapView.addAnnotation(objectAnnotation!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -77,6 +81,112 @@ class ViewController: UIViewController {
 //        mapView?.addOverlays(overlays)
     }
 
+    func showAlertSheet() {
+        let latitude = la
+        let longitude = lo
+        let alertController = UIAlertController(title: "选择导航地图", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction.init(title: "取消", style: .cancel) { (action) in
+            self.mapView.deselectAnnotation(self.objectAnnotation, animated: true)
+        }
+        //系统自带地图，内核高德地图，无需判断是否安装
+        let appleAction = UIAlertAction(title: "自带地图", style: .default){ (action) in
+            let currentLocation = MKMapItem.forCurrentLocation()
+            let toLocation = MKMapItem.init(placemark: MKPlacemark.init(coordinate: (self.objectAnnotation?.coordinate)!, addressDictionary: nil))
+            MKMapItem.openMaps(with: [currentLocation, toLocation],
+                               launchOptions: [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving,
+                                               MKLaunchOptionsShowsTrafficKey:true])
+        }
+        //百度地图
+        if UIApplication.shared.canOpenURL(URL.init(string: "baidumap://")!) {//判断是否安装了地图
+            let baiduAction = UIAlertAction(title: "百度地图", style: .default){ (action) in
+                //注意：origin={{我的位置}}不要变；目的地随便写
+                let urlString = "baidumap://map/direction?origin={{我的位置}}&destination=latlng:\(latitude),\(longitude)|name=目的地&mode=driving&coord_type=gcj02"
+                
+                let escapedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(URL.init(string: escapedString!)!, options: [:], completionHandler: nil)
+                } else {
+                    if UIApplication.shared.canOpenURL(URL.init(string: escapedString!)!) {
+                        UIApplication.shared.openURL(URL.init(string: escapedString!)!)
+                    }
+                }
+            }
+            alertController.addAction(baiduAction)
+        }
+        //高德地图
+        if UIApplication.shared.canOpenURL(URL.init(string: "iosamap://")!) {
+            let gaodeAction = UIAlertAction(title: "高德地图", style: .default){ (action) in
+                let urlString = "iosamap://navi?sourceApplication=随便写&backScheme=随便写&lat=\(latitude)&lon=\(longitude)&dev=0&style=2"
+                let escapedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(URL.init(string: escapedString!)!, options: [:], completionHandler: nil)
+                } else {
+                    if UIApplication.shared.canOpenURL(URL.init(string: escapedString!)!) {
+                        UIApplication.shared.openURL(URL.init(string: escapedString!)!)
+                    }
+                }
+            }
+            alertController.addAction(gaodeAction)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(appleAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+//
+//        let alertVc = UIAlertController.init(title: "请选择地图", message: nil, preferredStyle: .actionSheet)
+//        let action1 = UIAlertAction.init(title: "用百度地图导航", style: .default, handler: { (action) in
+//            self.goToBaiDuMap()
+//        })
+//        let action2 = UIAlertAction.init(title: "用高德地图导航", style: .default, handler: { (action) in
+//            self.goToGaoDeMap()
+//        })
+//        let cancleAction = UIAlertAction.init(title: "取消", style: .cancel, handler: { (action) in
+//
+//        })
+//        alertVc.addAction(action1)
+//        alertVc.addAction(action2)
+//        alertVc.addAction(cancleAction)
+//        self.present(alertVc, animated: true, completion: nil)
+    }
+    
+    private func goToBaiDuMap() {
+        //判断是否安装了百度地图
+        if UIApplication.shared.canOpenURL(URL.init(string: "baidumap://")!) {//判断是否安装了地图
+            let latitude = 32.029171
+            let longitude = 118.788231
+            let urlString = "baidumap://map/direction?origin={{我的位置}}&destination=latlng:\(latitude),\(longitude)|name=目的地&mode=driving&coord_type=gcj02"
+            let escapedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(URL.init(string: escapedString!)!, options: [:], completionHandler: nil)
+            } else {
+                if UIApplication.shared.canOpenURL(URL.init(string: escapedString!)!) {
+                    UIApplication.shared.openURL(URL.init(string: escapedString!)!)
+                }
+            }
+            
+        }
+    }
+    
+    //高德地图
+    private func goToGaoDeMap() {
+        if UIApplication.shared.canOpenURL(URL.init(string: "iosamap://")!) {
+            let latitude = 32.029171
+            let longitude = 118.788231
+            let urlString = "iosamap://path?sourceApplication=applicationName&sid=BGVIS1&sname=我的位置&did=BGVIS2&dname=终点&dlat=\(latitude)&dlon=\(longitude)=0&t=0"
+            let escapedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(URL.init(string: escapedString!)!, options: [:], completionHandler: nil)
+            } else {
+                if UIApplication.shared.canOpenURL(URL.init(string: escapedString!)!) {
+                    UIApplication.shared.openURL(URL.init(string: escapedString!)!)
+                }
+            }
+        } else {
+//            self.view.showMessage("未安装高德地图")
+        }
+    }
 }
 
 
@@ -87,41 +197,34 @@ extension ViewController: MKMapViewDelegate{
         if annotation is MKUserLocation {
             return nil
         }
-        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
-        if pinView == nil{
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
-            pinView?.canShowCallout = true
-//            pinView?.rightCalloutAccessoryView = UIButton(type: .contactAdd)
-            pinView?.image = UIImage(named: "card_map_icon1")
-        }else{
-            pinView?.annotation = annotation
-        }
-        return pinView
+//        var customView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
+       let customView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+        customView.canShowCallout = false
+        customView.image = UIImage(named: "card_map_icon1")
+//        if customView == nil{
+//
+//        }else{
+//            customView?.annotation = annotation
+//        }
+        return customView
+    }
+
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        view.image = UIImage(named: "card_map_icon1")
+//        if isLight{
+//            view.image = UIImage(named: "card_map_icon2")
+//        }else {
+//            view.image = UIImage(named: "card_map_icon1")
+//        }
+//        isLight = false
     }
     
-    func mapView(mapView: MKMapView!, didAddAnnotationViews views: [AnyObject]!) {
-//        println("添加注释视图")
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        view.image = UIImage(named: "card_map_icon2")
+//        isLight = true
+//        mapView.deselectAnnotation(objectAnnotation, animated: true)
+        showAlertSheet()
     }
     
-    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!,
-                 calloutAccessoryControlTapped control: UIControl!) {
-//        println("点击注释视图按钮")
-    }
-    
-    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
-//        println("点击大头针注释视图")
-    }
-    
-    func mapView(mapView: MKMapView!, didDeselectAnnotationView view: MKAnnotationView!) {
-//        println("取消点击大头针注释视图")
-    }
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let renderer = MKCircleRenderer(overlay: overlay)
-        renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
-        renderer.strokeColor = UIColor.blue
-        renderer.lineWidth = 2
-        return renderer
-    }
 }
 
