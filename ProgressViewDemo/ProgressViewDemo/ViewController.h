80f01ad25bca792ffe8d6c4c4217196c46f9a9b5
ICloud <zhengyun@yeah.net>
2018-08-27 18:02:39 +0800

    固件升级进度条动画demo ，现在是OC版本， 后期改成Swift版

---
+++ ProgressViewDemo/ProgressViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressViewDemo
+//
+//  Created by ICloud on 2018/8/27.
+//  Copyright © 2018年 ICloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
