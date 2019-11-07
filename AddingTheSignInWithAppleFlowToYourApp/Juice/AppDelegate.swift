1503c9fa066ff4d14f54aa6344a223fec343f1aa
ICloud <zhengyun@yeah.net>
2019-11-07 21:26:23 +0800

    ----sigin with apple

---
+++ AddingTheSignInWithAppleFlowToYourApp/Juice/AppDelegate.swift
@@ -0,0 +1,43 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main application delegate.
+*/
+
+import UIKit
+import AuthenticationServices
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ///判断本地是否注销
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                // The Apple ID credential is valid.
+                break
+            case .revoked:
+                // The Apple ID credential is revoked.
+                break
+            case .notFound:
+                // No credential was found, so show the sign-in UI.
+                DispatchQueue.main.async {
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    guard let viewController = storyboard.instantiateViewController(withIdentifier: "loginViewController") as? LoginViewController
+                        else { return }
+                    viewController.modalPresentationStyle = .formSheet
+                    viewController.isModalInPresentation = true
+                    self.window?.rootViewController?.present(viewController, animated: true, completion: nil)
+                }
+            default:
+                break
+            }
+        }
+        return true
+    }
+}
